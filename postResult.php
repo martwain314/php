b8c9f0e8db956740e1db889c26b11374af7f29f2
Eunchong Shin <martwain314@naver.com>
2016-01-14 15:34:34 +0900

    #6 POST 작동 연습

---
+++ postResult.php
@@ -0,0 +1,11 @@
+<?php
+ $name = $_POST['name'];
+ $year = $_POST['year'];
+ $mon = $_POST['mon'];
+ $day = $_POST['day'];
+
+?>
+ <h1>Wow!</h1>
+  I know you, <?=$name?>!! <br>
+  Your birthday is <?=$year?>-<?=$mon?>-<?=$day?><br>
+  Your age :  <?=$age?> years old.
